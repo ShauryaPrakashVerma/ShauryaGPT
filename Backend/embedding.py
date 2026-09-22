ad46bd8984854e5662674c373136db215b09ca34
Shaurya Prakash Verma <shauryaverma22@gmail.com>
2026-09-23 02:46:53 +0530

    Add basic embedding model

--- Backend/embedding.py
+++ Backend/embedding.py
@@ -1,3 +1,19 @@
+from sentence_transformers import SentenceTransformer
 
-def generate_embeddings():
-    pass
+model = SentenceTransformer(
+    "BAAI/bge-small-en-v1.5"
+)
+
+text = "Hello My name is shaurya. this is my text"
+
+def generate_embeddings(text):
+    
+    embedding = model.encode(text)
+    print(embedding)
+    
+    # return model.encode(
+    #     chunks,
+    #     normalize_embeddings=True
+    # )
+    
+generate_embeddings(text)
